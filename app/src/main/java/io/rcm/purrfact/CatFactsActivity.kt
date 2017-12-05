e75db142803c96fb5baf76b1ed856ec2281ddc82
Joice Marinay <joicemarinay@gmail.com>
2017-12-05 08:57:41 +0800

    Refactor: MainActivity to CatFactsActivity (and other components)

--- app/src/main/java/io/rcm/purrfact/CatFactsActivity.kt
+++ app/src/main/java/io/rcm/purrfact/CatFactsActivity.kt
@@ -3,10 +3,10 @@ package io.rcm.purrfact
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 
-class MainActivity : AppCompatActivity() {
+class CatFactsActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.catfacts_activity)
     }
 }
